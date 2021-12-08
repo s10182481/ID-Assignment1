ffaa061878775cc9a96ac65dd67bea71f521787d
Muhammad Faizal Bathurul J <s10182481@connect.np.edu.sg>
2021-12-08 22:08:21 +0800

    My Changes

---
+++ main.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("Sidenav").style.width = "50%";
+  document.getElementById("mask").style.width = "50%";
+}
+
+function closeNav() {
+  document.getElementById("Sidenav").style.width = "0";
+  document.getElementById("mask").style.width = "0";
+}
